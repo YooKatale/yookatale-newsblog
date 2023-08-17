c41b28e94d699c43160a3e44ed39ae70cb1c001d
AturiheihiBlendon <2020blendon@gmail.com>
2023-08-17 09:19:06 +0300

    Added a service worker for offline mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
 import Header from "@components/Header";
 import Posts from "@components/Posts";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      console.log('serviceWorker supported in browser')
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((reg) => console.log('serviceWorker registration', reg));
+    }
+  }, []);
+
   return (
     <main className="">
       <Header />
