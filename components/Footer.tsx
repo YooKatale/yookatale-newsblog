c019098ac53cc1ffff30079538f10c48b095d776
darlio88 <danielomoding173@gmail.com>
2023-08-22 00:35:15 +0300

    credits-daniel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,15 @@
 const Footer = () => {
   return (
-    <div className="bg-emerald-800 text-amber-400 py-5">
+   <>
+    <div className="py-5 bg-emerald-800 text-amber-400">
       <div className="w-10/12 mx-auto">
         <p className="text-center">© 2023 YOOKATALE All rights reserved</p>
       </div>
     </div>
+    <div className="py-2 text-sm text-center bg-slate-800 text-slate-200">
+      <p>Designed with love by <a className="mx-1 underline text-sky-400" target="blank" href='https://github.com/darlio88'>Daniel</a>  </p>
+    </div>
+   </>
   );
 };
 
